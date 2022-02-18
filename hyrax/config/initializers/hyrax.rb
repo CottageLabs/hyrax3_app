a50a2e6f72b748bcca35bfa36a05648a011e4cd3
Anusha Ranganathan <anusha@cottagelabs.com>
2022-02-18 05:45:06 +0000

    Created work type dataset

--- hyrax/config/initializers/hyrax.rb
+++ hyrax/config/initializers/hyrax.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 Hyrax.config do |config|
+  # Injected via `rails g hyrax:work Dataset`
+  config.register_curation_concern :dataset
   # Register roles that are expected by your implementation.
   # @see Hyrax::RoleRegistry for additional details.
   # @note there are magical roles as defined in Hyrax::RoleRegistry::MAGIC_ROLES
