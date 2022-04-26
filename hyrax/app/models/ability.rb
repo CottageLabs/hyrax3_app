e0ff4a30d53c09316c306db8790657620f9b10a3
Anusha Ranganathan <anusha@cottagelabs.com>
2022-04-26 23:45:04 +0100

    Add create content to abaility

--- hyrax/app/models/ability.rb
+++ hyrax/app/models/ability.rb
@@ -2,7 +2,10 @@ class Ability
   include Hydra::Ability
   
   include Hyrax::Ability
-  self.ability_logic += [:everyone_can_create_curation_concerns]
+  self.ability_logic += [
+    :everyone_can_create_curation_concerns,
+    :create_content
+  ]
 
   # Define any customized permissions here.
   def custom_permissions
@@ -17,5 +20,14 @@ class Ability
     # if user_groups.include? 'special_group'
     #   can [:create], ActiveFedora::Base
     # end
+    if current_user.admin?
+      can [:create, :show, :add_user, :remove_user, :index, :edit, :update, :destroy], Role
+    end
+  end
+
+  def create_content
+    # everyone who is logged in can create content
+    # return unless registered_user?
+    can :create, ::Dataset if current_user
   end
 end
