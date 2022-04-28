0e17efa139e535a7fc7941d3b4d6c09f9527368a
Anusha Ranganathan <anusha@cottagelabs.com>
2022-04-28 13:42:06 +0100

    Removed dataset work type

--- hyrax/app/forms/hyrax/dataset_form.rb
+++
@@ -1,9 +0,0 @@
-# Generated via
-#  `rails generate hyrax:work Dataset`
-module Hyrax
-  # Generated form for Dataset
-  class DatasetForm < Hyrax::Forms::WorkForm
-    self.model_class = ::Dataset
-    self.terms += [:resource_type]
-  end
-end
--- hyrax/app/presenters/hyrax/dataset_presenter.rb
+++
@@ -1,6 +0,0 @@
-# Generated via
-#  `rails generate hyrax:work Dataset`
-module Hyrax
-  class DatasetPresenter < Hyrax::WorkShowPresenter
-  end
-end
