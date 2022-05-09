0b735912186431f04d1b2d394c5f85e4a5a87ef5
kapill65chhpatel <kapil@cottagelabs.com>
2022-05-10 00:35:59 +0530

    Remove Unused Code

--- hyrax/spec/spec_helper.rb
+++ hyrax/spec/spec_helper.rb
@@ -95,29 +95,25 @@ RSpec.configure do |config|
 
   # setup selenium  for unit test
 
-  require 'selenium-webdriver'
   require 'capybara'
+  require 'selenium-webdriver'
 
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome()
-  Capybara.register_driver :selenium_chrome_headless_sandboxless do |app|
-    driver = Capybara::Selenium::Driver.new(app,
-                                            browser: :remote,
-                                            desired_capabilities: capabilities,
-                                            url: ENV['HUB_URL'])
+  Capybara.register_driver :selenium_chrome_headless do |app|
+    chrome_options = Selenium::WebDriver::Chrome::Options.new
 
-    # Fix for capybara vs remote files. Selenium handles this for us
-    driver.browser.file_detector = lambda do |args|
-      str = args.first.to_s
-      str if File.exist?(str)
-    end
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--window-size=1400,1400')
 
-    driver.maximize_window driver.current_window_handle
-    driver
+    driver = Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options )
   end
 
-  Capybara.server_host = '0.0.0.0'
-  Capybara.server_port = 3010
-  ip = IPSocket.getaddress(Socket.gethostname)
-  Capybara.app_host = "http://#{ip}:#{Capybara.server_port}"
-  Capybara.javascript_driver = :selenium_chrome_headless_sandboxless # This is slower
+  Capybara.configure do |config|
+    config.server_host = '0.0.0.0'
+    config.server_port = 3010
+    config.app_host = "http://#{IPSocket.getaddress(Socket.gethostname)}:#{Capybara.server_port}"
+    config.javascript_driver = :selenium_chrome_headless
+  end
 end
