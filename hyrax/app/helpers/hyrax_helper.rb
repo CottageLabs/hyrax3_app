32258d10c1627aa2ee0a48e361afdde7e26d88d9
Anusha Ranganathan <anusha@cottagelabs.com>
2023-01-05 01:33:04 +0000

    Hyrax helper was missing

--- hyrax/app/helpers/hyrax_helper.rb
+++ hyrax/app/helpers/hyrax_helper.rb
@@ -3,4 +3,9 @@ module HyraxHelper
   include ::BlacklightHelper
   include Hyrax::BlacklightOverride
   include Hyrax::HyraxHelperBehavior
+  include ::SdgService
+
+  def sdg_facet_display(id)
+    SdgService.label(id)
+  end
 end
